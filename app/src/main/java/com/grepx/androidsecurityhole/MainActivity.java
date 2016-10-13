e5b6071369d9d11e7c1ad257964835e6e1c298ef
Greg Pearce <gregrpearce@gmail.com>
2016-10-13 10:05:21 +0100

    Refactor to use menu rather than button

--- app/src/main/java/com/grepx/androidsecurityhole/MainActivity.java
+++ app/src/main/java/com/grepx/androidsecurityhole/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -32,14 +33,22 @@ public class MainActivity extends AppCompatActivity {
         Picasso.with(this)
                 .load("http://i.imgur.com/9QkX4Jy.png")
                 .into(kitten3);
+    }
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LogActivity.class);
-                startActivity(intent);
-            }
-        });
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_view_log) {
+            Intent intent = new Intent(MainActivity.this, LogActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
