8537fe7e648f9b10963062a0d5221f8664a63b5d
Greg Pearce <gregrpearce@gmail.com>
2016-10-13 09:56:43 +0100

    Add check for password manager.

--- app/src/main/java/com/grepx/androidsecurityhole/ClipboardWatcherService.java
+++ app/src/main/java/com/grepx/androidsecurityhole/ClipboardWatcherService.java
@@ -5,7 +5,11 @@ import android.content.ClipData;
 import android.content.ClipDescription;
 import android.content.ClipboardManager;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.IBinder;
+
+import java.util.List;
 
 public class ClipboardWatcherService extends Service {
 
@@ -21,7 +25,29 @@ public class ClipboardWatcherService extends Service {
     @Override
     public void onCreate() {
         ((ClipboardManager) getSystemService(CLIPBOARD_SERVICE)).addPrimaryClipChangedListener(listener);
+        checkForPasswordManager();
         serviceIsRunning = true;
+    }
+
+    /**
+     * Checks to see if a password manager is installed and logs it.
+     * <p/>
+     * In a real app, more advanced optimisation would be possible using this information.
+     * For example, detect passwords that match the default generation settings for that manager.
+     * <p/>
+     * You could also transmit a full list of apps installed on the device to know which services
+     * the user uses.
+     */
+    private void checkForPasswordManager() {
+        final PackageManager pm = getPackageManager();
+        List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        for (ApplicationInfo packageInfo : packages) {
+            if (packageInfo.packageName.equals("com.agilebits.onepassword") ||
+                        packageInfo.packageName.equals("com.lastpass.lpandroid")) {
+                log.append(packageInfo.packageName);
+                log.append(" is installed.\n\n");
+            }
+        }
     }
 
     @Override
